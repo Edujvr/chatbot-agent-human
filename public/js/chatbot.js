30eb3cc43473b44f6fc445a6ebbe02773ab6328e
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-11-09 12:25:27 -0500

    Update chatbot.js

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -30,7 +30,7 @@ $(function () {
     	for(i=0;i<len;i++){
 		var obj = msg[i];
 		if(msg[i]['message']==='text'){
-                	 speech=obj['text']['text'][i].textToSpeech;
+                	 speech=obj['text']['text'][i];
             }
 	}  	  
 	var messages=response.queryResult.fulfillmentMessages; 
