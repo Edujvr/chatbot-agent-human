8e5a94c94247c7aa6de25d940ef90b8520eeafc4
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-11-14 09:55:05 -0500

    Update chatbot.js

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -52,9 +52,9 @@ $(function () {
         $(answerRow).append(answerCol);
         $(answerCol).append(answerContainerDiv);
         var textFromDefaultResponse = speech;
-        if (textFromDefaultResponse.trim()!==''){
+        /*if (textFromDefaultResponse.trim()!==''){
             renderDefaultResponse(textFromDefaultResponse,answerContainerDiv);
-        }
+        }*/
 	renderRichControls(messages, answerContainerDiv);
         var isDisabled = $('#m').prop('disabled');
         if(eoc){
